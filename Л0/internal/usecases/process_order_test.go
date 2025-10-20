094b9159c0e79b557b04db9c0cc71fd58837975a
tvbondar <sinopahfox@yandex.ru>
2025-10-20 16:27:18 +0300

    feat(main): upload patch

--- Л0/internal/usecases/process_order_test.go
+++ Л0/internal/usecases/process_order_test.go
@@ -18,7 +18,17 @@ func TestProcessOrderUseCase_SaveSuccess(t *testing.T) {
 	mockCache := mockrepos.NewMockOrderRepository(ctrl)
 
 	// Подготовим корректный JSON (минимально)
-	raw := []byte(`{"order_uid":"o1","track_number":"t1","delivery":{"name":"n","phone":"p"},"payment":{"transaction":"tx","currency":"USD","provider":"p","amount":1},"items":[{"chrt_id":1,"track_number":"t1","price":1,"name":"n","total_price":1,"nm_id":2,"brand":"b"}],"locale":"en","customer_id":"c","delivery_service":"ds","date_created":"2021-11-26T06:22:19Z"}`)
+	raw := []byte(`{
+		"order_uid":"o1",
+		"track_number":"t1",
+		"delivery":{"name":"n","phone":"p"},
+		"payment":{"transaction":"tx","currency":"USD","provider":"p","amount":1},
+		"items":[{"chrt_id":1,"track_number":"t1","price":1,"name":"n","total_price":1,"nm_id":2,"brand":"b"}],
+		"locale":"en",
+		"customer_id":"c",
+		"delivery_service":"ds",
+		"date_created":"2021-11-26T06:22:19Z"
+	}`)
 
 	// ожидания: DB.SaveOrder вызовется, затем Cache.SaveOrder вызовется
 	mockDB.EXPECT().SaveOrder(gomock.Any(), gomock.Any()).Return(nil).Times(1)
