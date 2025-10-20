f26970f5a599be474c25b57e7da154685504d1b8
tvbondar <sinopahfox@yandex.ru>
2025-10-20 16:24:41 +0300

    Delete Л0/internal/repositories/order_repository.go

--- Л0/internal/repositories/order_repository.go
+++
@@ -1,9 +0,0 @@
-package repositories
-
-import "github.com/tvbondar/go-server/internal/entities"
-
-type OrderRepository interface {
-	SaveOrder(order entities.Order) error
-	GetOrderByID(id string) (entities.Order, error)
-	GetAllOrders() ([]entities.Order, error) // Для восстановления кэша
-}
